408880bb8de043044cc71cd9a623a46f2446e4c7
sarath sasidharan <sasidharansharath13@gmail.com>
2023-09-15 17:39:27 +0530

    Commit

--- client/src/pages/Blogs.js
+++ client/src/pages/Blogs.js
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BlogCard from "../components/BlogCard";
+
 const Blogs = () => {
-  return <div>Blogs</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  // Get Blogs
+
+  const getAllBlogs = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/blogs/get-blogs");
+      if (data?.success) {
+        setBlogs(data?.blogs);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllBlogs();
+  }, []);
+  return (
+    <>
+      <BlogCard />
+    </>
+  );
 };
 export default Blogs;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -42,62 +42,66 @@ const Login = () => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <Box
-          maxWidth={450}
-          display={"flex"}
-          flexDirection={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          margin={"auto"}
-          marginTop={5}
-          boxShadow={"10px 10px 20px #ccc"}
-          padding={3}
-          borderRadius={5}
-        >
-          <Typography
-            variant="h4"
-            sx={{ textTransform: "uppercase" }}
+        <div style={{ display: "flex" }}>
+          <img src="login.png" style={{ flex: 0 }} />
+          <Box
+            maxWidth={450}
+            display={"flex"}
+            flex={3}
+            flexDirection={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            margin={"auto"}
+            marginTop={5}
+            boxShadow={"10px 10px 20px #ccc"}
             padding={3}
-            textAlign={"center"}
+            borderRadius={5}
           >
-            Login
-          </Typography>
+            <Typography
+              variant="h4"
+              sx={{ textTransform: "uppercase" }}
+              padding={3}
+              textAlign={"center"}
+            >
+              Login
+            </Typography>
 
-          <TextField
-            placeholder="email"
-            value={inputs.email}
-            onChange={handleChange}
-            name="email"
-            margin="normal"
-            type="email"
-            required
-          />
-          <TextField
-            placeholder="password"
-            value={inputs.password}
-            onChange={handleChange}
-            name="password"
-            margin="normal"
-            type="password"
-            required
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            sx={{ borderRadius: "30px", marginTop: 3 }}
-          >
-            Submit
-          </Button>
-          <Button
-            onClick={() => navigate("/register")}
-            type="submit"
-            color="primary"
-            sx={{ borderRadius: "30px", marginTop: 3 }}
-          >
-            Not Registered ? Please Register
-          </Button>
-        </Box>
+            <TextField
+              placeholder="email"
+              value={inputs.email}
+              onChange={handleChange}
+              name="email"
+              margin="normal"
+              type="email"
+              required
+            />
+            <TextField
+              placeholder="password"
+              value={inputs.password}
+              onChange={handleChange}
+              name="password"
+              margin="normal"
+              type="password"
+              required
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ borderRadius: "30px", marginTop: 3 }}
+            >
+              Submit
+            </Button>
+            <Button
+              onClick={() => navigate("/register")}
+              type="submit"
+              color="primary"
+              sx={{ borderRadius: "30px", marginTop: 3 }}
+            >
+              Not a user ? Please Register
+            </Button>
+          </Box>
+        </div>
       </form>
     </>
   );
